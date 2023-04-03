d637e05753563de6c844b75370aa6eaaeeb85735
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2023-04-03 22:03:30 +0300

    clesed scanner

--- TeamLongestPeriod-app/team.longest.period/src/team/longest/period/main/TeamLongestPeriodApplication.java
+++ TeamLongestPeriod-app/team.longest.period/src/team/longest/period/main/TeamLongestPeriodApplication.java
@@ -11,13 +11,14 @@ public class TeamLongestPeriodApplication {
     public static void main(String[] args) {
         
     	// Get file path
-        File file = new File(TeamLongestPeriodApplication.class.getResource("employee-records.csv").getFile());
+        File file = new File(TeamLongestPeriodApplication.class.getResource("employee-records-second-format.csv").getFile());
         String filePath = file.getAbsolutePath();
         
-        // All data formats can be set here 
+        // All data formats can be set with console UI 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the date format string (e.g. yyyy-MM-dd): ");
         String dateFormat = scanner.nextLine();
+        scanner.close();
         
         // Set the repository with data from the "csv" file (comma separated)        
         TeamLongestPeriodRepository repository = new TeamLongestPeriodRepositoryImpl(filePath);        
